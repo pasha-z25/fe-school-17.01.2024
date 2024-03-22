2807d30ffa9ecd8423a997facb5f9efca5548090
Pavlo Zubak <pashazubak@gmail.com>
2024-03-23 00:44:55 +0200

    add basic functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log('It works!');
+
+const userNameTarget = document.getElementById('userName');
+
+function formHandler(event) {
+    event.preventDefault();
+
+    // ... some code ...
+}
+
+function toggleSections(hasUser = false) {
+    // ... some code ...
+}
+
+function getFromStorage(name) {
+    // ... some code ...
+}
