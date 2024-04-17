d420c80577494fd9430753d7fb5c49870892ade4
Pasha Zubak <pashazubak@gmail.com>
2024-04-17 19:23:25 +0300

    some

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('It works!');
+
+$('.burger').click(function () {
+  $(this).toggleClass('active');
+  $('.nav').toggleClass('active');
+});
+
+$('.popup').click(function () {
+  $('.popup-wrapper').addClass('active');
+})
+
+$('.popup-window .close, .popup-wrapper').click(function (event) {
+  if (event.target === this) {
+    $('.popup-wrapper').removeClass('active');
+  }
+})
